db95137e66ae005760c0c60299b2fa514cf1abfb
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2024-07-09 18:27:26 +0300

    Add button to change grid resolution

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-const sketchContainer = document.querySelector("#sketch-container");
+const sketchContainer = document.querySelector(".sketch-container");
+const changeResolutionBtn = document.querySelector(".change-grid-btn");
 const GRID_SIZE= 960;
-
-let gridResolution = 16;
-let pixelSize = GRID_SIZE / gridResolution;
-let numeberOfPixels = gridResolution**2;
-
-for(let i = 0; i < numeberOfPixels; i++){
-    const gridPixel = document.createElement("div");
-    gridPixel.style.width = `${pixelSize}px`;
-    gridPixel.style.height = `${pixelSize}px`;
-    gridPixel.style.border = "1px solid black";
-    gridPixel.style.boxSizing = "border-box";
-    sketchContainer.appendChild(gridPixel);
+if(!sessionStorage.getItem("resolution")){
+    sessionStorage.setItem("resolution", 16);
 }
 
-const pixelsArray = document.querySelectorAll("#sketch-container > div");
+function buildGrid(gridResolution){
+    let numeberOfPixels = gridResolution**2;
+    let pixelSize = GRID_SIZE / gridResolution;
 
-pixelsArray.forEach(pixel => {
-    pixel.addEventListener("mouseenter", () => {
-        pixel.style.backgroundColor = "red";
+    for(let i = 0; i < numeberOfPixels; i++){
+        const gridPixel = document.createElement("div");
+        gridPixel.style.width = `${pixelSize}px`;
+        gridPixel.style.height = `${pixelSize}px`;
+        gridPixel.style.border = "1px solid black";
+        gridPixel.style.boxSizing = "border-box";
+        sketchContainer.appendChild(gridPixel);
+    }
+}
+
+function addPixelHoverListeners(){
+    const pixelsArray = document.querySelectorAll(".sketch-container > div");
+
+    pixelsArray.forEach(pixel => {
+        pixel.addEventListener("mouseenter", (event) => {
+            pixel.style.backgroundColor = "red";
+        });
     });
+}
+
+buildGrid(sessionStorage.getItem("resolution"));
+
+addPixelHoverListeners();
+
+changeResolutionBtn.addEventListener("click", () => {
+    let newResolution = prompt("Enter new grid resolution (Max 64 | Min 2)", "");
+    if(newResolution > 64 || newResolution < 2){
+        alert("Incorrect resolution!");
+        return;
+    }
+    sketchContainer.innerHTML = "";
+    buildGrid(newResolution);
+    addPixelHoverListeners();
+    sessionStorage.setItem("resolution", newResolution);
 });
